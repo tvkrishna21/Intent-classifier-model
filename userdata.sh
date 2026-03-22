c7adaf9f1389c83ff15e3108cb24fd378038a89d
tvkrishna21 <76438536+tvkrishna21@users.noreply.github.com>
2026-03-22 22:59:03 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -19,7 +19,7 @@ cd $APP_DIR
 apt update -y
 apt install -y git python3 python3-venv python3-pip nginx
 
-git clone https://github.com/iam-veeramalla/Intent-classifier-model.git .
+git clone https://github.com/tvkrishna21/Intent-classifier-model.git .
 
 python3 -m venv .venv
 source .venv/bin/activate
